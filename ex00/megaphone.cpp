d3821a90871bc212d25d146afda118e7f579ac02
kpuwar <kpuwar@student.42heilbronn.de>
2023-10-29 23:07:36 +0100

    ex00 done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,23 +6,32 @@
 /*   By: kpuwar <kpuwar@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 18:01:57 by kpuwar            #+#    #+#             */
-/*   Updated: 2023/09/05 18:13:15 by kpuwar           ###   ########.fr       */
+/*   Updated: 2023/10/24 21:34:35 by kpuwar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
+#include <string>
 
-int main(int argc, char const *argv[])
+using std::cout;
+using std::endl;
+using std::string;
+
+string toupper_string(string str) {
+	for (size_t i = 0; i < str.length(); i++)
+		str[i] = std::toupper(str[i]);
+	return (str);
+}
+
+int main(int argc, char *argv[])
 {
 	if (argc == 1) {
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
 	} else {
-		for (unsigned short i = 1; argv[i]; i++) {
-			for (unsigned short j = 0; j < strlen(argv[i]); j++) {
-				std::cout << (char) std::toupper(argv[i][j]);
-			}
-		}
-		std::cout << std::endl;		
+		for (size_t i = 1; argv[i] != NULL; i++)
+			cout << toupper_string(argv[i]);
+		cout << endl;
 	}
-	return 0;
+	return (0);
 }
